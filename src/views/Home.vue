630f31060180680ef0ed7b9050199d4ed438273f
Jane <chenjira.k@kisracorp.com>
2021-03-29 17:04:34 +0700

    edit constant

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,22 @@
-<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+<template>
   <div>
-    <v-row justify="center">
-      <v-dialog
-              v-model="dialog"
-              persistent
-              max-width="600px"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-                  color="primary"
-                  dark
-                  v-bind="attrs"
-                  v-on="on"
-          >
-            Open Dialog
-          </v-btn>
-        </template>
-        <profile-modal @submit="saveData"></profile-modal>
-      </v-dialog>
-    </v-row>
+      can not template
   </div>
 </template>
 
 <script>
-  import ProfileModal from "../components/ProfileModal";
   export default {
     name: 'Home',
 
     components: {
-        ProfileModal,
+
     },
       data(){
         return {
-          dialog : false
         }
       },
       methods:{
-        saveData(value){
-            this.dialog = value;
-        }
+
       }
   }
 </script>
