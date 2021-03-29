6695b5c2296bec40a5631a3157149ce5eaaf8ad0
Jane <chenjira.k@kisracorp.com>
2021-03-29 21:10:13 +0700

    go to https

--- src/ConstantAPI.js
+++ src/ConstantAPI.js
@@ -1,2 +1,2 @@
 // export const domain = "http://127.0.0.1:8000/api";
-export const domain = "http://gomap-restaurant.herokuapp.com/public/api/";
+export const domain = "https://gomap-restaurant.herokuapp.com/public/api/";
